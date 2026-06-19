dbc16094b2781df7f5e7f2d8eca5ac32fdde613a
dvlproductionsllc-hub <dvlproductionsllc@gmail.com>
2026-06-19 16:28:37 -0700

    Update storage.js

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -1,14 +1,27 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+const MOOD_KEY = "planner_mood";
+
 export async function saveMood(mood) {
-  await AsyncStorage.setItem(
-    "currentMood",
-    mood
-  );
+  try {
+    await AsyncStorage.setItem(
+      MOOD_KEY,
+      JSON.stringify(mood)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-export async function getMood() {
-  return await AsyncStorage.getItem(
-    "currentMood"
-  );
+export async function loadMood() {
+  try {
+    const mood = await AsyncStorage.getItem(
+      MOOD_KEY
+    );
+
+    return mood ? JSON.parse(mood) : null;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 }
